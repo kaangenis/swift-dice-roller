97ab2bbf5aae373ecfda54de4df6f125c2cd953e
Kaan Genis <kaang18@gmail.com>
2024-04-15 14:28:25 +0300

    Initial Commit

---
+++ SwiftDiceRoller/SwiftDiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDiceRollerApp.swift
+//  SwiftDiceRoller
+//
+//  Created by Kaan Geniş on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
